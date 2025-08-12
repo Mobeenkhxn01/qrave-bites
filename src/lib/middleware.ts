5cff4b94eee267eda91765eeeb9f9ee027c32522
Mobeenkhxn01 <mobeenkhan191915@gmail.com>
2025-08-13 01:04:39 +0530

    Middleware updated

--- src/lib/middleware.ts
+++ src/lib/middleware.ts
@@ -1,1 +1,22 @@
-export { auth as middleware } from "@/lib/auth"
+// middleware.ts
+import { auth } from "@/lib/auth";
+import { NextResponse } from "next/server";
+
+export default auth((req) => {
+  const { nextUrl } = req;
+  if (!req.auth && nextUrl.pathname.startsWith("/dashboard")) {
+    return NextResponse.redirect(new URL("/login", req.url));
+  }
+
+  // Admin route protection
+  if (
+    req.auth?.user.role !== "admin" &&
+    nextUrl.pathname.startsWith("/admin")
+  ) {
+    return NextResponse.redirect(new URL("/unauthorized", req.url));
+  }
+});
+
+export const config = {
+  matcher: ["/((?!api|_next/static|_next/image|favicon.ico).*)","/dashboard/:path*", "/admin/:path*"],
+}

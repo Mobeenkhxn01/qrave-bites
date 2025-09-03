423ba827c21c81235c53042597e3e119b2aa7a14
Mobeenkhxn01 <mobeenkhan191915@gmail.com>
2025-09-03 17:45:29 +0530

    user profile

--- src/components/layout/navbar/UserMenu.tsx
+++ src/components/layout/navbar/UserMenu.tsx
@@ -5,21 +5,27 @@ import { Button } from "@/components/ui/button";
 import { useSession, signOut } from "next-auth/react";
 import { useQuery } from "@tanstack/react-query";
 import axios from "axios";
+import { Avatar, AvatarFallback, AvatarImage } from "@/components/ui/avatar";
+import {
+  DropdownMenu,
+  DropdownMenuContent,
+  DropdownMenuItem,
+  DropdownMenuLabel,
+  DropdownMenuSeparator,
+  DropdownMenuTrigger,
+} from "@/components/ui/dropdown-menu";
 
 const fetchUserProfile = async () => {
   const response = await axios.get("/api/profile");
   return response.data;
 };
-
 export default function UserMenu() {
   const { data: session } = useSession();
-
   const { data: userProfile, isLoading } = useQuery({
     queryKey: ["userProfile"],
     queryFn: fetchUserProfile,
     enabled: !!session,
   });
-
   if (!session) {
     return (
       <Link href="/login">
@@ -29,18 +35,36 @@ export default function UserMenu() {
       </Link>
     );
   }
-
   return (
     <>
-      <Link href="/profile" className="text-black font-serif">
-        {isLoading ? "Loading..." : `Hello, ${userProfile?.name || "User"}`}
-      </Link>
-      <Button
-        onClick={() => signOut()}
-        className="font-serif text-white bg-[#eb0029] hover:bg-[#d19b6f]"
-      >
-        Logout
-      </Button>
+      <DropdownMenu>
+        <DropdownMenuTrigger>
+          <Avatar>
+            <AvatarImage src={session?.user?.image || ""} />
+            <AvatarFallback>CN</AvatarFallback>
+          </Avatar>
+        </DropdownMenuTrigger>
+        <DropdownMenuContent>
+          <DropdownMenuLabel>
+            {isLoading ? "Loading..." : `Hello, ${userProfile?.name || "User"}`}
+          </DropdownMenuLabel>
+          <DropdownMenuSeparator />
+          <DropdownMenuItem>
+            <Link href="/profile">Profile</Link>
+          </DropdownMenuItem>
+          <DropdownMenuItem>
+            <Link href={"/settings"}>Settings</Link>
+          </DropdownMenuItem>
+          <DropdownMenuItem>
+            <Button
+              onClick={() => signOut()}
+              className="font-serif text-white bg-[#eb0029] hover:bg-[#d19b6f]"
+            >
+              Logout
+            </Button>
+          </DropdownMenuItem>
+        </DropdownMenuContent>
+      </DropdownMenu>
     </>
   );
 }

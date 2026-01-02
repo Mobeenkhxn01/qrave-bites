74f6e690a2e5e078cb78788010cd1e4a39776c5b
Mobeenkhxn01 <mobeenkhan191915@gmail.com>
2026-01-02 17:04:18 +0530

    chore: upgrade nextjs to fix security vulnerability

--- src/lib/middleware.ts
+++ src/lib/middleware.ts
@@ -1,22 +1,30 @@
-import { auth } from "@/lib/auth";
 import { NextResponse } from "next/server";
+import { getToken } from "next-auth/jwt";
+import type { NextRequest } from "next/server";
 
-export default auth((req) => {
-  const { nextUrl } = req;
+export async function middleware(req: NextRequest) {
+  const token = await getToken({
+    req,
+    secret: process.env.AUTH_SECRET,
+  });
 
-  if (!req.auth && nextUrl.pathname.startsWith("/dashboard")) {
+  const { pathname } = req.nextUrl;
+
+
+  if (!token && pathname.startsWith("/dashboard")) {
     return NextResponse.redirect(new URL("/login", req.url));
   }
 
   if (
-    req.auth &&
-    req.auth.user.role !== "ADMIN" &&
-    req.auth.user.role !== "RESTAURANT_OWNER" &&
-    nextUrl.pathname.startsWith("/dashboard")
+    token &&
+    !["ADMIN", "RESTAURANT_OWNER"].includes(token.role as string) &&
+    pathname.startsWith("/dashboard")
   ) {
     return NextResponse.redirect(new URL("/unauthorized", req.url));
   }
-});
+
+  return NextResponse.next();
+}
 
 export const config = {
   matcher: ["/dashboard/:path*"],

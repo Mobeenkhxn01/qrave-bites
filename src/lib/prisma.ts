6de47b1b97f2d26d058863df734686e2ae9e8b6c
Mobeenkhxn01 <mobeenkhan191915@gmail.com>
2026-01-02 06:05:38 +0530

    linting error resolving

--- src/lib/prisma.ts
+++ src/lib/prisma.ts
@@ -1,9 +1,16 @@
-import { PrismaClient } from "@prisma/client"
-import { withAccelerate } from "@prisma/extension-accelerate"
- 
-const globalForPrisma = globalThis as unknown as { prisma: PrismaClient }
- 
+import Prisma from "@prisma/client";
+import { withAccelerate } from "@prisma/extension-accelerate";
+
+const { PrismaClient } = Prisma;
+
+const globalForPrisma = globalThis as unknown as {
+  prisma: InstanceType<typeof PrismaClient>;
+};
+
 export const prisma =
-  globalForPrisma.prisma || new PrismaClient().$extends(withAccelerate())
- 
-if (process.env.NODE_ENV !== "production") globalForPrisma.prisma = prisma
+  globalForPrisma.prisma ??
+  new PrismaClient().$extends(withAccelerate());
+
+if (process.env.NODE_ENV !== "production") {
+  globalForPrisma.prisma = prisma;
+}
